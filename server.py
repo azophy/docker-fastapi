9b95f22baf4006fc8518288d48e5db2e4267cb5c
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-10-27 11:41:22 +0700

    Update server to display ip & hostname + add dockerignore

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-import os
+import os, socket
 from fastapi import FastAPI
 
 app = FastAPI()
 
 APP_NAME = os.getenv('APP_NAME', 'MY API')
+  
+def get_Host_name_IP(): 
+    """
+    Function to display hostname and IP address 
+    ref: https://www.geeksforgeeks.org/display-hostname-ip-address-python/
+    """
+    try: 
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        return {
+            "Hostname" : host_name,
+            "IP" : host_ip,
+        }
+    except: 
+        return "Unable to get Hostname and IP"
 
 @app.get("/")
 async def root():
     return {
         "message": "Hello World",
         "APP_NAME" : APP_NAME,
+        'host_info': get_Host_name_IP(),
     }
